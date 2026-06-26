719b056a80a6f788741620c945e070a1043df399
ReynaldoBahe <reynaldo.bahe1@gmail.com>
2026-06-26 19:09:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ st.sidebar.write("---")
 arquivo_upload = st.sidebar.file_uploader("📂 Carregar Planilha de Ativos/OM", type=["csv", "xlsx"])
 
 # URL base do Speckle original aprovado
-speckle_base_url = "https://speckle.systems"
+speckle_base_url = "https://app.speckle.systems/projects/a649da7292/models/815af390c7?embedToken=5fc6fc722186f65bfe3c4be3286713af5a1ab94df3"
 
 # Lógica de carregamento de dados segura
 df = pd.DataFrame()
