174a00183d42cc1e7a052bd9c4ea76bcc3110c8a
ReynaldoBahe <reynaldo.bahe1@gmail.com>
2026-06-28 16:45:53 -0300

    Update 00_Login.py

--- pages/00_Login.py
+++ pages/00_Login.py
@@ -4,25 +4,33 @@ st.set_page_config(page_title="Login", page_icon="🔐", layout="centered")
 
 st.title("Acesso ao Sistema")
 
+# CSS para estilizar nativamente o checkbox do Streamlit em ciano e aumentar a fonte
+st.markdown(
+    """
+    <style>
+    /* Altera a cor do texto do checkbox nativo e aumenta o tamanho */
+    div[data-testid="stCheckbox"] label p {
+        color: #00ffff !important;
+        font-size: 18px !important;
+        font-weight: bold !important;
+    }
+    /* Opcional: Altera a cor da borda/fundo do quadrado do checkbox quando marcado */
+    div[data-testid="stCheckbox"] input[type="checkbox"]:checked + div {
+        background-color: #00ffff !important;
+        border-color: #00ffff !important;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 with st.form(key="login_form"):
     username = st.text_input("Usuário", placeholder="Digite seu usuário")
     password = st.text_input("Senha", type="password", placeholder="Digite sua senha")
     
-    st.markdown(
-        """
-        <style>
-        .grande-ciano {
-            font-size: 20px !important;
-            color: #00ffff !important;
-            font-weight: bold;
-        }
-        </style>
-        <span class="grande-ciano">Lembrar de mim</span>
-        """, 
-        unsafe_allow_html=True
-    )
+    # O checkbox agora já nasce com o texto grande e ciano integrado de forma nativa
+    lembrar = st.checkbox("Lembrar de mim")
     
-    lembrar = st.checkbox("Ativar a opção acima", label_visibility="collapsed")
     submit_button = st.form_submit_button(label="Entrar")
 
 if submit_button:
